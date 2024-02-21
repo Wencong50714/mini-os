e41c8cd4b46ddc32d5df5dfbbf52f10f16e7ea6d
Ryan <czh50714@gmail.com>
2024-02-21 10:52:53 +0800

    post-03 again

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-#![no_std]
-#![no_main]
+
+
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    loop {}
-}
+mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
     loop {}
 }
