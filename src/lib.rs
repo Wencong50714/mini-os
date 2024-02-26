931191e5cae5e13ef0478de24d4f5fd323717fd7
Ryan <czh50714@gmail.com>
2024-02-27 00:42:16 +0800

    post-05 finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
@@ -8,6 +9,7 @@ use core::panic::PanicInfo;
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -59,8 +61,13 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ use mini_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    mini_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
